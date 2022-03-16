89c04fc8bf1a8beab47b453c5880d258c7707d6f
qn19325 <qn19325@bristol.ac.uk>
2022-03-16 12:47:30 +0000

    rnn so far only passes sequences of 64x64

--- sim/test.py
+++ sim/test.py
@@ -10,24 +10,26 @@ env = UnityToGymWrapper(unity_env, uint8_visual=True, allow_multiple_obs=True)
 env.reset()
 myData = []
 index = 0
-for _ in range(252):
+for _ in range(253):
         action = env.action_space.sample()
         obs, reward, done, info = env.step(np.abs(2*action))
         arrayObs = np.array(obs[0])
-        myData.append((arrayObs, obs[1], index))
+        myData.append((arrayObs, np.array(obs[1]).item(), index))
         index += 1
 
-with open('data.csv', 'w') as f:
-        writer = csv.writer(f)
-        for x in range(len(myData)):
-                pixels = []
-                for i in range(64):
-                        row = myData[x][0][i].flatten()
-                        pixels.append(row)
-                        index = myData[x][1]
-                array = np.array(pixels, dtype=np.uint8)
-                new_image = Image.fromarray(array)
-                new_image.save('training/' + str(x) + '.png')
-                row = [x, pixels, myData[x][1].item()]
-                writer.writerow(row)
+for x in range(len(myData)):
+        pixels = []
+        for i in range(64):
+                row = myData[x][0][i].flatten()
+                pixels.append(row)
+                index = myData[x][1]
+        array = np.array(pixels, dtype=np.uint8)
+        new_image = Image.fromarray(array)
+        new_image.save('training/' + str(x) + '.png')
 
+with open('data.csv', 'w', newline='') as file:
+        for i in range(len(myData)):
+                writer = csv.writer(file)
+                speed = myData[i][1]
+                path = str(i) + ".png"
+                writer.writerow([path, speed])
