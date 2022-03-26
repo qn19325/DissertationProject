57d66fd34805dc72ed1c849d1aaa8838ca5518f4
qn19325 <qn19325@bristol.ac.uk>
2022-03-26 13:16:20 +0000

    add models.py

--- cnn_rnn.py
+++ cnn_rnn.py
@@ -9,46 +9,46 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-device = 'cpu'
-
 class ImageDataLoader(Dataset):
     def __init__(self, dir_=None):
-        self.data_df = pd.read_csv('data.csv')
+        self.data_df = pd.read_csv('gdrive/MyDrive/data.csv')
         self.dataset_len = len(self.data_df) # read the number of len of your csv files
     def __getitem__(self, idx):
         # load the next image
         f_name_t = self.data_df['Filename'][idx]
         f_name_tp1 = self.data_df['Filename'][idx+1]
         label = self.data_df['Label'][idx]
-        img_t = torchvision.io.read_image('trainingData/{}'.format(f_name_t))
-        img_tp1 = torchvision.io.read_image('trainingData/{}'.format(f_name_tp1))
-        img_t = img_t.flatten(1,-1).float().div_(255.0)
-        img_tp1 = img_tp1.flatten(1,-1).float().div_(255.0)
-        input_seq = torch.cat((img_t, img_tp1), dim=0)
-        return input_seq, label
+        img_t = torchvision.io.read_image('gdrive/MyDrive/trainingData/{}'.format(f_name_t))
+        img_tp1 = torchvision.io.read_image('gdrive/MyDrive/trainingData/{}'.format(f_name_tp1))
+        img_t = img_t.float().div_(255.0)
+        img_tp1 = img_tp1.float().div_(255.0)
+        return img_t, img_tp1, label
     def __len__(self):
         return self.dataset_len - 1
 
 dataset = ImageDataLoader()
 dataloader = DataLoader(dataset, shuffle=False, batch_size=8)
 
-def imshow(img):
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 16, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, 1, 1),
+            nn.ReLU(),
+            nn.Flatten()
+        )
+    def forward(self, x):
+        state = self.cnn(x)
+        # print('size of the state after CNN ',state.size())
+        return state
 
-imagesList = []
-for idx, (images, labels) in enumerate(dataloader):
-  images = images.reshape(-1,1,64,64)
-  imagesList.append(images)
-
-randomBatch = random.randrange(0,len(imagesList))
-print('Batch:', randomBatch)
-imshow(torchvision.utils.make_grid(imagesList[randomBatch]))
+encoder = Encoder()
 
 num_epochs = 10
 learning_rate = 0.001
-input_size = 4096
+input_size = 131072
 output_size = 1
 sequence_length = 2
 hidden_size = 128
@@ -63,7 +63,7 @@ class RNN(nn.Module):
         self.fc = nn.Linear(hidden_size, output_size)
         
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size) 
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
         out, _ = self.rnn(x, h0)  
         # out = out[:, -1]
         out = self.fc(out)
@@ -74,19 +74,20 @@ model = RNN(input_size, hidden_size, num_layers, output_size)
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-def get_accuracy(logit, target, batch_size):
-    ''' Obtain accuracy for training round '''
-    corrects = (torch.max(logit, 1)[1].view(target.size()).data == target.data).sum()
-    accuracy = 100.0 * corrects/batch_size
-    return accuracy.item()
-
 n_total_steps = len(dataloader)
 for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(dataloader):
-        labels = (labels.float()).to(device)
+    for i, (image1, image2, label) in enumerate(dataloader):
+        image1, image2, label = next(iter(dataloader))
+        output = encoder(image1)
+        output1 = encoder(image2)
+        output = output.reshape(8,1,-1)
+        output1 = output1.reshape(8,1,-1)
+        seq = torch.cat((output, output1), dim=1)
+        label = (label.float())
+
         # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs[:,-1], labels)
+        outputs = model(seq)
+        loss = criterion(outputs[:,-1], label)
         
         # Backward and optimize
         optimizer.zero_grad()
