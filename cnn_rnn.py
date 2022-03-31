b3cda2e47b9e696be8ee8ebd3b786177b323ce47
qn19325 <qn19325@bristol.ac.uk>
2022-03-31 16:29:04 +0100

    updated cnn_rnn.py

--- cnn_rnn.py
+++ cnn_rnn.py
@@ -16,8 +16,8 @@ class ImageDataLoader(Dataset):
         label = self.data_df['Label'][idx]
         label = label.astype(np.float32) 
         label = np.true_divide(label, 10)
-        img_t = torchvision.io.read_image('trainingData/{}'.format(f_name_t))
-        img_tp1 = torchvision.io.read_image('trainingData/{}'.format(f_name_tp1))
+        img_t = torchvision.io.read_image('training/{}'.format(f_name_t))
+        img_tp1 = torchvision.io.read_image('training/{}'.format(f_name_tp1))
         img_t = img_t.float().div_(255.0)
         img_tp1 = img_tp1.float().div_(255.0)
         return img_t, img_tp1, label
@@ -25,33 +25,35 @@ class ImageDataLoader(Dataset):
         return self.dataset_len - 1
 
 dataset = ImageDataLoader()
-dataloader = DataLoader(dataset, shuffle=False, batch_size=8)
+dataloader = DataLoader(dataset, shuffle=True, batch_size=8)
 
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
         self.cnn = nn.Sequential(
-            nn.Conv2d(1, 16, 1, 1),
+            nn.Conv2d(1, 8, 1, 1),
             nn.ReLU(),
-            nn.Conv2d(16, 32, 1, 1),
+            nn.Conv2d(8, 16, 1, 1),
             nn.ReLU(),
             nn.Flatten()
         )
+        self.fc1 = nn.Linear(65536, 128)
     def forward(self, x):
         state = self.cnn(x)
+        state = self.fc1(state)
         # print('size of the state after CNN ',state.size())
         return state
 
 encoder = Encoder()
 
 batch_size = 8
-num_epochs = 100
+num_epochs = 1000
 learning_rate = 0.001
-input_size = 131072
+input_size = 128
 output_size = 1
 sequence_length = 2
-hidden_size = 256
-num_layers = 28
+hidden_size = 128
+num_layers = 1
 
 class RNN(nn.Module):
     def __init__(self, batch_size, input_size, hidden_size, num_layers, output_size):
@@ -61,13 +63,10 @@ class RNN(nn.Module):
         self.num_layers = num_layers
         self.hidden_size = hidden_size
         self.output_size = output_size
-
         self.rnn = nn.RNN(self.input_size, self.hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(self.hidden_size, self.output_size)
-
     def init_hidden(self):
         return (torch.zeros(self.num_layers, self.batch_size, self.hidden_size))
-
     def forward(self, x):
         self.batch_size = x.size(0)
         self.hidden = self.init_hidden()
@@ -86,21 +85,25 @@ for epoch in range(num_epochs):
     for i, (image1, image2, label) in enumerate(dataloader):
         output1 = encoder(image1)
         output2 = encoder(image2)
+        # print('o1 shape:', output1.size())
         batch_size1 = len(output1)
         batch_size2 = len(output2)
         output1 = output1.reshape(batch_size1,1,-1)
         output2 = output2.reshape(batch_size2,1,-1)
-        seq = torch.cat((output1, output2), dim=1)
+        # print('o1 after shape:', output1.size())
+        seq = torch.cat((output1, output2.detach()), dim=1)
+        # print('seq size: ', seq.size())
         label = (label.float())
-
         # Forward pass
         outputs = model(seq)
         loss = criterion(outputs[:,-1].squeeze(), label)
-        print(outputs[:,-1].squeeze())
-        print(label)
+        # print(outputs[:,-1].squeeze())
+        # print(label)
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         runningLoss += loss
-        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+    if epoch%10 == 0:
+        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {runningLoss:.4f}')
+    runningLoss = 0
