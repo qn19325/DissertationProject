981f44b9b58126c5727172d418d60a7ba011b654
qn19325 <qn19325@bristol.ac.uk>
2022-04-20 18:18:54 +0100

    add training data

---
+++ animation.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from PIL import Image
+import numpy as np
+
+imgs = []
+
+for i in range(500):
+    image = Image.open('speed8_512x512/' + str(i) + '.png')
+    pixels = list(image.getdata())
+    array = np.array(pixels)
+    shapedArray = array.reshape(512,512)
+    imgs.append(shapedArray)
+
+fig, ax = plt.subplots()
+
+x = np.linspace(0, 512)
+y = np.linspace(0, 512).reshape(-1, 1)
+
+ims = []
+for i in range(500):
+    im = ax.imshow(imgs[i], animated=True)
+    if i == 0:
+        ax.imshow(imgs[0])  # show an initial one first
+    ims.append([im])
+
+ani = animation.ArtistAnimation(fig, ims, interval=200, blit=True,
+                                repeat_delay=1000)
+
+ani.save("movie.mp4")
+
+plt.show()
